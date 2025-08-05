1759d2a4276b188d907d3dfda847e1d6a6322925
NarendarTrainer <narendartrainer@gmail.com>
2025-08-05 15:49:22 +0530

    stage7->create / /me apis

--- backend/app/main.py
+++ backend/app/main.py
@@ -1,10 +1,10 @@
 from fastapi import FastAPI
-from .routers import auth
+from .routers import auth,users
 app=FastAPI( title="EdTech Adaptive Learning Platform API",
     version="1.0.0",
     description="Backend service for personalized learning paths, assessments, and feedback.")
 app.include_router(auth.router,prefix="/api/v1/auth",tags=["Auth"])
-
+app.include_router(users.router,prefix="/api/v1/users",tags=["Users"])
 @app.get("/")
 async def root():
     return {"message":"Welcome to EdTech Adaptive Learning Platform API"}
